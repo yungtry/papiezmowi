ab749519efdf41fde99e9e1863294626a406d840
Kacper <trybikk@gmail.com>
2016-02-22 20:11:13 +0100

    poprawa reinstalacji

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ wget https://raw.githubusercontent.com/yungtry/papiezmowi/master/papiezmowi
 chmod +x papiezmowi
 printf '\n\nPotrzebuje uprawnien zeby móc zainstalować papiezmowi\n'
 sudo rm /usr/local/bin/papiezmowi
+sudo rm /usr/local/bin/papiez-papa
 sudo mv papiezmowi /usr/local/bin
 wget https://raw.githubusercontent.com/yungtry/papiezmowi/master/papiez-papa
 chmod +x papiez-papa
