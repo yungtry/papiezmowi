ccb2a1853ce5528d71844f1b1e8f0a587f4400e2
Kacper <trybikk@gmail.com>
2016-02-22 20:03:25 +0100

    updated script

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ chmod +x papiezmowi
 printf '\n\nPotrzebuje uprawnien zeby móc zainstalować papiezmowi\n'
 sudo rm /usr/local/bin/papiezmowi
 sudo mv papiezmowi /usr/local/bin
-
+wget https://raw.githubusercontent.com/yungtry/papiezmowi/master/papiez-papa
+chmod +x papiez-papa
+sudo mv papiez-papa /usr/local/bin
